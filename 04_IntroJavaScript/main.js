4d920220255447e46f4b6ab53aecaefa96c949ae
Kelebra28 <ricardokelebra@gmail.com>
2019-10-01 10:24:59 -0500

    Challenge FlexBox & Intro a JavaScript

---
+++ 04_IntroJavaScript/main.js
@@ -0,0 +1,32 @@
+// alert("DESCARGA YA!!!!!!");
+
+//Valor numerico
+let numero = 10;
+//Valor String
+let texto = 'Hola me llamo Ricardo';
+//Valor Boolean
+let buleano = true;
+// ----------------------------> Variables especiales
+// Array 
+let arreglo = [];
+//object
+let objeto = {};
+
+// console.log( "Saludo desde la consola" );
+
+// let x = 100;
+// console.log(x)
+
+// x = 1
+
+// console.log(x);
+
+// x = "XD"
+
+// console.log(x);
+
+let name =  prompt("Ingresa tu nombre");
+console.log("Hola " + name);
+
+//Operadores
+//  + - / *
